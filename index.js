6440e775127c0baf370a0caee4798628c6632b29
reeyansu <reeyansus@gmail.com>
2024-09-24 13:19:07 +0200

    second assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("toggleButton").onclick = function() {
+    var textElement = document.getElementById("textElement");
+    textElement.textContent = textElement.textContent === "This is the original text" ?
+     "This is the changed text" : "This is the original text";
+    this.textContent = this.textContent === "Change" ? "Show Original" : "Change";
+}
+var oldText = "New Inputed Text"
+function changeText()
+{
+    article= document.getElementById
+}
+index.js
